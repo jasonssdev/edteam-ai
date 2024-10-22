e7c82d87f0b08cac84907641c55150f3a8f3b56e
Jasonssdev <jasonssdev@gmail.com>
2024-10-21 20:21:30 -0500

    chatbot course

--- chatbot/interface.py
+++ chatbot/interface.py
@@ -1,15 +1,24 @@
 import gradio as gr
+from transformers import pipeline
 
-def greet(name, part_of_the_day, tempeture_f):
-    part_of_the_day_dict = {'morning':'Goog morning', 'afternoon': 'Good afternoon', 'night': 'Good night'}
-    temperature = round((tempeture_f - 32) * 5/9, 2)
-    if part_of_the_day in part_of_the_day_dict:
-        return f'{part_of_the_day_dict[part_of_the_day]} {name}, current temperature is {temperature} celsius degrees'
+generator = pipeline('text-generation', model='gpt2')
+
+def greet(text):
+    result = generator(text, max_length=50, num_return_sequences=1)
+    return result[0]["generated_text"]  
+
+
+examples = [
+    ["The Moon's orbit around Earth has"],
+    ["The smooth Borealis basin in the Northern Hemisphere cover 40%"]
+]
 
 app = gr.Interface(
     fn=greet,
-    inputs=["text", "text", gr.Slider(0,100)],
-    outputs='text'
-    )
+    inputs=gr.Textbox(lines=5, label="Text"),
+    outputs=gr.Textbox(label='Text'),
+    examples=examples 
+)
 
+# Lanza la aplicación
 app.launch()
